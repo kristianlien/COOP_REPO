6e99c1577158b246c475808f1a27d4ce96d651ca
Kristian Lien <krnlien@gmail.com>
2024-09-24 13:19:21 +0200

    la til gjennomsnitt back-end

--- back_end.py
+++ back_end.py
@@ -12,8 +12,8 @@ def trekkFra():
     print(f'{tall2} - {tall2} = {diff}')
 
 def gange():
-    tall1 = input("Skriv inn det første tallet")
-    tall2 = input("Skriv inn det andre tallet:")
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("Skriv inn det andre tallet: ")
     produkt = tall1 * tall2
     print(f"{tall1} + {tall2} = {produkt}")
 
@@ -26,3 +26,10 @@ def dele():
     tall2 = input("Skriv inn det andre tallet: ")
     diff = int(tall1) / int(tall2)
     print(f'{tall2} / {tall2} = {diff}')
+
+def gjennomsnitt():
+    tall1 = input("Skriv inn det første tallet: ")
+    tall2 = input("sSriv inn det andre tallet: ")
+    sum = tall1 + tall2
+    gjs = sum / 2
+    print(f"gjennomsnittet mellom {tall1} og {tall2} er {gjs}")
